9470dde75c3f455731fdb9d9a74e53f785fb0b82
Michael Kåring <michael@mkd.no>
2018-05-16 00:01:08 +0200

    Fixed throttling of downloads to prevent computerfreeze

--- secrets.js
+++ secrets.js
@@ -1,31 +1,34 @@
 var exports = module.exports = {};
 var request = require('request');
-var _ = require('underscore');
 var moment = require('moment');
 var fs = require('fs');
+var async = require('async');
 
 exports.fetch = function(secretsData, dir, callback){
   var count = 0;
 
   var finalCount = secretsData.length;
 
-  _.each(secretsData, function(item){
+  // Run only 5 downloads at a time.
+  async.eachOfLimit(secretsData, 5, function(item, key, cb){
     request.get({
-      url: item.file,
-    })
-    .on("error", function(error) {
-        
-    })
-    .on('response',  function (res) {
-      res.pipe(fs.createWriteStream(dir+'/Couple/Secrets/From '+item.fromName+'/'+item.date +'.'+ res.headers['content-type'].split('/')[1]));
-      res.on( 'end', function(){
-        count = count+1;
-        callback({
-          count: count,
-          finished: (count == finalCount)?true:false
+        url: item.file,
+      })
+      .on("error", function(error) {
+        console.log(error);
+      })
+      .on('response',  function (res) {
+        res.pipe(fs.createWriteStream(dir+'/Couple/Secrets/From '+item.fromName+'/'+item.date +'.'+ res.headers['content-type'].split('/')[1]));
+        res.on( 'end', function(){
+          count = count+1;
+          callback({
+            count: count,
+            finished: (count == finalCount)?true:false
+          });
+          cb();
+          console.log('moment downloaded');
         });
       });
-    });
   });
 
 };
--- timeline.js
+++ timeline.js
@@ -3,6 +3,7 @@ var request = require('request');
 var _ = require('underscore');
 var moment = require('moment');
 var fs = require('fs');
+var base64 = require('base-64');
 
 exports.fetch = function(timelineUrl, userObject, dir, callback){
     request.get({
@@ -18,14 +19,25 @@ exports.fetch = function(timelineUrl, userObject, dir, callback){
             entry.date = moment(entry.timeStamp).format('YYYY-MM-DD HH mm ss');
             if(entry.file && !entry.secret){ entry.localDir = dir+'/Couple/From '+entry.fromName; }
             if(entry.file && entry.secret == 1){ entry.localDir = dir+'/Couple/Secrets/From '+entry.fromName; }
-            if(entry.enc && entry.enc == "b64" && entry.text){ entry.text = Buffer.from(entry.text, 'base64').toString("ascii"); }
+            if(entry.enc && entry.enc == "b64" && entry.text){ entry.text = base64.decode(entry.text); }
           });
 
           var CoupleDir = dir+'/Couple';
 
-          fs.writeFileSync(CoupleDir+'/Timeline.json', JSON.stringify(timeline));
+          fs.writeFile(CoupleDir+'/Timeline.json', JSON.stringify(timeline), 'utf8', function (err) {
+              if (err) {
+                  return console.log(err);
+              }
 
-          callback(timeline);
+              console.log("The file was saved!");
+          });
+
+          callback({
+            moments: _.filter(timeline, function(item){ return item.eventType === "media" && item.file; }),
+            secrets: _.filter(timeline, function(item){ return item.secret === 1 && item.file; })
+          });
+
+          timeline = null;
         }
       });
 };
